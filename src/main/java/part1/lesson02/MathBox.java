c336c4c398aece9f727dc7ebdfe1176b46bee139
Алексей Винокуров <avv96@yandex.ru>
2020-07-30 18:40:04 +0300

    [Part1][Lesson02] small fixes

--- src/main/java/part1/lesson02/MathBox.java
+++ src/main/java/part1/lesson02/MathBox.java
@@ -2,13 +2,15 @@ package part1.lesson02;
 
 import part1.lesson02.Calculators.*;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
 public class MathBox<T extends Number> extends ObjectBox<T>{
-    Class<T> clazz;
+    Calculator<T> calculator;
+
     Map<Class<?>, Calculator<?>> map = new HashMap<>(){{
         put(Integer.class, new IntegerCalculator());
         put(Double.class, new DoubleCalculator());
@@ -17,28 +19,31 @@ public class MathBox<T extends Number> extends ObjectBox<T>{
         put(Short.class, new ShortCalculator());
         put(Byte.class, new ByteCalculator());
     }};
+
     public MathBox(T[] arr) {
         if (arr.length != 0) {
-            clazz = (Class<T>) arr[0].getClass();
+            Class<T> clazz = (Class<T>) arr[0].getClass();
+            if (map.containsKey(clazz)) {
+                calculator = (Calculator<T>) map.get(clazz);
+            }
         }
         for (T item : arr) {
-            super.addObject(item);
+            addObject(item);
+        }
+    }
+
+    public MathBox(T[] arr, Calculator<T> calculator) {
+        this.calculator = calculator;
+        for (T item : arr) {
+            addObject(item);
         }
     }
 
     public T summator() {
-        return sumValue((Calculator<T>) map.get(clazz));
-    }
-
-    public void splitter(T item) {
-        divideList((Calculator<T>) map.get(clazz), (T) item);
-    }
-
-    public T sumValue(Calculator<T> calculator) {
         return getList().stream().reduce(calculator.zero(), calculator::add);
     }
 
-    public void divideList(Calculator<T> calculator, T item) {
+    public void splitter(T item) {
         List<T> list = getList();
         for (int i = 0; i < list.size(); i++) {
             list.set(i, calculator.divide(list.get(i), item));
