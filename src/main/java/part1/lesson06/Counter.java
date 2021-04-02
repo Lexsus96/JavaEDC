703101586f3d420a86152edce212525d4f93b2fa
Алексей Винокуров <avv96@yandex.ru>
2021-04-02 15:12:59 +0300

    [Part1][Lesson06] Add hashMap, remove get, add join

--- src/main/java/part1/lesson06/Counter.java
+++ src/main/java/part1/lesson06/Counter.java
@@ -5,14 +5,20 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
 public class Counter {
     private int N_THREADS = 8;
+    private Map<Integer, BigInteger> hashMap = new HashMap<>();
+
     Counter() {
 
     }
     Counter(int N_THREADS) {
         this.N_THREADS = N_THREADS;
     }
+
     public BigInteger count(int start, int end) {
         BigInteger sum = BigInteger.ONE;
         for (int i = start + 1; i <= end; i++) {
@@ -20,12 +26,26 @@ public class Counter {
         }
         return sum;
     }
+    
+    public static <T> CompletableFuture<List<T>> allOf(
+            Collection<CompletableFuture<T>> futures) {
+        return futures.stream()
+                .collect(collectingAndThen(
+                        toList(),
+                        l -> CompletableFuture.allOf(l.toArray(new CompletableFuture[0]))
+                                .thenApply(__ -> l.stream()
+                                        .map(CompletableFuture::join)
+                                        .collect(Collectors.toList()))));
+    }
 
     public Map<Integer, BigInteger> execute(List<Integer> list) throws ExecutionException, InterruptedException {
         Map<Integer, BigInteger> map = new HashMap<>();
-        list = list.stream().distinct().sorted().collect(Collectors.toList());
+
+        list = list.stream().distinct().sorted().collect(toList());
+        list.stream().filter(i -> hashMap.containsKey(i)).forEach(i -> map.put(i, hashMap.get(i)));
+        list = list.stream().filter(i -> !hashMap.containsKey(i)).collect(toList());
         ExecutorService threadPool = Executors.newFixedThreadPool(N_THREADS);
-        List<Future<BigInteger>> answer = new ArrayList<>();
+        List<CompletableFuture<BigInteger>> answer = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             int start, end;
             if (i == 0) {
@@ -36,11 +56,12 @@ public class Counter {
             end = list.get(i);
             answer.add(CompletableFuture.supplyAsync(() -> count(start, end), threadPool));
         }
-
+        List<BigInteger> l = allOf(answer).join();
         BigInteger currentDigit = BigInteger.ONE;
         for (int i = 0; i < answer.size(); i++) {
-            currentDigit = currentDigit.multiply(answer.get(i).get());
+            currentDigit = currentDigit.multiply(l.get(i));
             map.put(list.get(i), currentDigit);
+            hashMap.putIfAbsent(list.get(i), currentDigit);
         }
 
         threadPool.shutdown();
--- src/main/java/part1/lesson06/Solution.java
+++ src/main/java/part1/lesson06/Solution.java
@@ -8,12 +8,20 @@ import java.util.stream.Stream;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static List<Integer> generateList(int max_n, int size) {
         Random gen = new Random();
-        List<Integer> list = Stream.generate(() -> gen.nextInt(50)).limit(100).collect(Collectors.toList());
+        return Stream.generate(() -> gen.nextInt(max_n)).limit(size).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> list = generateList(1000, 100);
         Counter counter = new Counter();
         try {
             Map<Integer, BigInteger> map = counter.execute(list);
+            System.out.println(map);
+            list = generateList(50, 100);
+            map = counter.execute(list);
             System.out.println(map);
         } catch (Exception e) {
             System.out.println(e.getMessage());
